82dd7c689b033bd950d803cd0800020a5a35dbce
Eduardo Vázquez López <eduardo.vazlopez@iberant.es>
2020-09-30 16:03:04 -0400

    Add FizzBuzz component

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,7 +1,8 @@
+import 'antd/dist/antd.css';
 import React, { Component } from 'react';
 import { Route } from 'react-router';
 import { Layout } from './components/Layout';
-import { Home } from './components/Home';
+import { FizzBuzz } from './components/FizzBuzz';
 import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
 
@@ -13,7 +14,7 @@ export default class App extends Component {
   render () {
     return (
       <Layout>
-        <Route exact path='/' component={Home} />
+            <Route exact path='/' component={FizzBuzz} />
         <Route path='/counter' component={Counter} />
         <Route path='/fetch-data' component={FetchData} />
       </Layout>
---
+++ ClientApp/src/components/FizzBuzz.jsx
@@ -0,0 +1,39 @@
+﻿import React, { Component } from "react";
+import { Button } from "antd";
+import axios from "axios";
+
+export class FizzBuzz extends Component {
+    state = {
+        FizzBuzzList: [],
+    };
+
+    handleExecuteFizzBuzz = async () => {
+        const { data: response } = await axios.get(
+            "https://localhost:5001/api/fizzbuzz"
+        );
+        this.setState({ FizzBuzzList: response });
+        console.log("Hola Eduardo", response);
+    };
+
+    handleCleanBoard = () => this.setState({ FizzBuzzList: [] });
+
+    render() {
+        return (
+            <div>
+                <Button onClick={this.handleExecuteFizzBuzz} type="primary">
+                    Execute FizzBuzz Test
+        </Button>
+                <Button onClick={this.handleCleanBoard} type="primary">
+                    Clean Board
+        </Button>
+                <ul>
+                    {this.state.FizzBuzzList.map((i, index) => (
+                        <li key={index}>{i}</li>
+                    ))}
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default FizzBuzz;
