c5bf2334f4a8bbb5034b680578c37a156c1ea0a1
Eduardo Vázquez López <eduardo.vazlopez@iberant.es>
2020-09-30 14:17:59 -0400

    Add FizzBuzz service

---
+++ Services/FizzBuzzService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FizzBuzzProject.Services
+{
+    public static class FizzBuzzService
+    {
+        public static void ExecuteFizzBuzz(IList<string> result, int length = 100)
+        {
+            for (int i = 1; i < length; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    result.Add("FizzBuzz");
+                    continue;
+                }
+                if (i % 3 == 0)
+                {
+                    result.Add("Fizz");
+                    continue;
+                }
+                if (i % 5 == 0)
+                {
+                    result.Add("Buzz");
+                    continue;
+                }
+
+                result.Add(Convert.ToString(i));
+            }
+        }
+    }
+}
