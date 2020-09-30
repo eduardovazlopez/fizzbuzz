6b58cf1b446649dde0c8b05754626725a335ce5b
Eduardo Vázquez López <eduardo.vazlopez@iberant.es>
2020-09-30 13:48:38 -0400

    Add HomeController

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FizzBuzzProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("/api/fizzbuzz")]
+        public ActionResult<string> Get()
+        {
+            return Ok(new string[] { "value1", "value2" });
+        }
+
+    }
+}
