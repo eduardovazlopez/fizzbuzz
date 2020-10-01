88927d3292b2f0489e86372a20e7f3698c640818
Eduardo Vázquez López <eduardo.vazlopez@iberant.es>
2020-10-01 12:50:38 -0400

    Add FizzBuzzPage for Page Object Model

---
+++ src/FizzBuzzProject.E2ETests/Pages/FizzBuzzPage.cs
@@ -0,0 +1,17 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FizzBuzzProject.E2ETests.Pages
+{
+    public class FizzBuzzPage
+    {
+        private IWebDriver _driver;
+        public IList<IWebElement> fizzBuzzResults => _driver.FindElements(By.Name("FBli"));
+        public IWebElement runTestButton => _driver.FindElement(By.Id("RunTestButton"));
+
+        public FizzBuzzPage(IWebDriver driver) => _driver = driver;
+        public void GotoFizzBuzzPage() => _driver.Url = "https://localhost:5001/";
+    }
+}
